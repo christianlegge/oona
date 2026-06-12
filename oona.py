5db11cdb536487bfc990f33c9789fdc258899871
Christian Legge <christian@christianlegge.dev>
2026-06-12 01:48:16 -0400

    add garfield, nancyartist

--- oona.py
+++ oona.py
@@ -3,10 +3,13 @@ import datetime
 import discord
 from discord.ext import commands
 import os
+from random import randint
 
 DISCORD_KEY = os.environ.get("DISCORD_KEY")
 
 all_comics = comics.directory.listall()
+
+nancy_start = comics.directory.get_start_date("nancy")
 
 bot = commands.Bot()
 
@@ -88,6 +91,22 @@ async def foxtrot(ctx, date):
 
 
 @bot.slash_command(
+    name="nancyclassic",
+    description=f"pull up a Nancy Classics strip",
+    options=[
+        discord.Option(
+            input_type=discord.SlashCommandOptionType.string,
+            name="date",
+            description="[yyyy-mm-dd] pull a specific date (random if omitted)",
+            required=False,
+        ),
+    ],
+)
+async def nancyclassic(ctx, date):
+    await get_random(ctx, "nancy-classics", date)
+
+
+@bot.slash_command(
     name="nancy",
     description=f"pull up a Nancy strip",
     options=[
@@ -101,6 +120,45 @@ async def foxtrot(ctx, date):
 )
 async def nancy(ctx, date):
     await get_random(ctx, "nancy", date)
+
+
+@bot.slash_command(
+    name="garfield",
+    description=f"pull up a Garfield strip",
+    options=[
+        discord.Option(
+            input_type=discord.SlashCommandOptionType.string,
+            name="date",
+            description="[yyyy-mm-dd] pull a specific date (random if omitted)",
+            required=False,
+        ),
+    ],
+)
+async def garfield(ctx, date):
+    await get_random(ctx, "garfield", date)
+
+
+@bot.slash_command(
+    name="nancyartist",
+    description=f"pull up a Nancy strip by a specified artist",
+    options=[
+        discord.Option(
+            input_type=discord.SlashCommandOptionType.string,
+            name="artist",
+            description="artist to pull (probably not 100% accurate)",
+            required=True,
+            choices=["bushmiller", "gilchrist", "jaimes", "cash"],
+        ),
+    ],
+)
+async def nancyartist(ctx, artist):
+    daterange = {
+        "bushmiller": (nancy_start, "1995-09-02"),
+        "gilchrist": ("1995-09-04", "2018-02-18"),
+        "jaimes": ("2018-04-09", "2025-12-31"),
+        "cash": ("2026-01-01", datetime.datetime.now().strftime("%Y-%m-%d")),
+    }[artist]
+    await get_random(ctx, "nancy", None, daterange)
 
 
 @bot.slash_command(
@@ -122,13 +180,25 @@ async def nancy(ctx, date):
         ),
     ],
 )
-async def get_random(ctx, endpoint, date):
+async def get_random(ctx, endpoint, date, daterange=None):
     await ctx.defer()
     i = 0
     while True:
         i += 1
         try:
-            r = comics.search(endpoint, date="random" if date is None else date)
+            effectivedate = None
+            if daterange is not None:
+                start = datetime.datetime.strptime(daterange[0], "%Y-%m-%d")
+                end = datetime.datetime.strptime(daterange[1], "%Y-%m-%d")
+                rand_days = randint(0, (end - start).days)
+                effectivedate = (start + datetime.timedelta(days=rand_days)).strftime(
+                    "%Y-%m-%d"
+                )
+            elif date is not None:
+                effectivedate = date
+            r = comics.search(
+                endpoint, date="random" if effectivedate is None else effectivedate
+            )
             r.image_url
             break
         except:
