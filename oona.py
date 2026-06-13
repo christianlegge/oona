8a2230b944b9d6def8e95e5d99af628f49167659
Christian Legge <christian@christianlegge.dev>
2026-06-13 01:10:30 -0400

    add pearls command

--- oona.py
+++ oona.py
@@ -59,6 +59,22 @@ def style_comic(name, date, url, image_url):
 
 
 @bot.slash_command(
+    name="pearls",
+    description=f"pull up a Pearls Before Swine strip",
+    options=[
+        discord.Option(
+            input_type=discord.SlashCommandOptionType.string,
+            name="date",
+            description="[yyyy-mm-dd] pull a specific date (random if omitted)",
+            required=False,
+        ),
+    ],
+)
+async def pearls(ctx, date):
+    await get_random(ctx, "pearlsbeforeswine", date)
+
+
+@bot.slash_command(
     name="cathy",
     description=f"pull up a Cathy strip",
     options=[
